ca6c6b33c1852ce3756ad57ffb1a368529aa6c6b
Francisco Alvarado <ronald.alvarado@epn.edu.ec>
2020-09-26 17:13:25 -0500

    Examen

--- 01-http/introduccion/src/app.controller.ts
+++ 01-http/introduccion/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import {Body, Controller, Get, Post, Req, Res, Session} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,68 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('login')
+  login(
+      @Res() res
+  ){
+    return res.render('login/login');
+  }
+
+  @Post('login')
+  loginPost(
+      @Body() parametrosCuerpo,
+      @Res() res,
+      @Session() session
+  ){
+    // Validamos los datos
+    const usuario = parametrosCuerpo.usuario;
+    const password = parametrosCuerpo.password;
+    if(usuario === 'ronald' && password === '1234'){
+      session.usuario = usuario;
+      session.roles = ['Administrador'];
+      return res.redirect('protegido');
+    }else{
+      if(usuario === 'franciso' && password === '9876'){
+        session.usuario = usuario;
+        session.roles = ['Supervisor'];
+        return res.redirect('protegido');
+      }else{
+        return res.redirect('/login');
+      }
+    }
+  }
+
+  @Get('protegido')
+  protegido(
+      @Res() res,
+      @Session() session
+  ){
+    const estaLogeado = session.usuario;
+    if(estaLogeado){
+      return res.render(
+          'login/protegido',
+          {
+            usuario: session.usuario,
+            roles: session.roles
+          }
+      )
+    }else{
+      return res.redirect('/login');
+    }
+  }
+
+  @Get('logout')
+  logout(
+      @Session() session,
+      @Res() res,
+      @Req() req
+  ){
+    session.username = undefined;
+    session.roles = undefined;
+    req.session.destroy();
+    return res.redirect('login')
+  }
+
+
 }
